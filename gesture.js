997db682385fcb72efc05e9f3f492e062ef6d859
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-08-02 13:02:10 +0900

    [Update] 複数回実行しても最後の1回のイベントのみ有効

--- gesture.js
+++ gesture.js
@@ -1,40 +1,51 @@
-const Buttons = {
-    Right: 2
-};
-let rightButtonPressing = false;
-let gestureStartPosition = {
-    x: 0,
-    y: 0
-};
+// 2回目以降の実行の場合、以前のイベントをキャンセル
+// 開発用
+if (window.cancelPrevious) cancelPrevious();
+window.cancelPrevious = (function () {
+    const Buttons = {
+        Right: 2
+    };
+    let rightButtonPressing = false;
+    let gestureStartPosition = {
+        x: 0,
+        y: 0
+    };
 
-const thresholdX = 50; // [px] 
+    const thresholdX = 50; // [px]
 
-document.addEventListener("mousedown", evt => {
-    if (evt.button === Buttons.Right) {
-        rightButtonPressing = true;
-        gestureStartPosition.x = evt.clientX;
-        gestureStartPosition.y = evt.clientY;
-    } else {
-        rightButtonPressing = false;
-    }
-    
-});
-
-document.addEventListener("contextmenu", evt => {
-    if (rightButtonPressing) {
-        if (evt.clientX < 0 || evt.clientY < 0) {
-            // ウィンドウ外
-            return;
+    const onmousedown = evt => {
+        if (evt.button === Buttons.Right) {
+            rightButtonPressing = true;
+            gestureStartPosition.x = evt.clientX;
+            gestureStartPosition.y = evt.clientY;
+        } else {
+            rightButtonPressing = false;
         }
-        const diffX = evt.clientX - gestureStartPosition.x;
-        if (diffX < -thresholdX) {
-            // 左方向へ移動：戻る
-            evt.preventDefault();
-            window.history.back();
-        } else if (thresholdX < diffX) {
-            // 右方向へ移動：進む
-            evt.preventDefault();
-            window.history.forward();
+    };
+    document.addEventListener("mousedown", onmousedown);
+
+    const oncontextmenu = evt => {
+        if (rightButtonPressing) {
+            if (evt.clientX < 0 || evt.clientY < 0) {
+                // ウィンドウ外
+                return;
+            }
+            const diffX = evt.clientX - gestureStartPosition.x;
+            if (diffX < -thresholdX) {
+                // 左方向へ移動：戻る
+                evt.preventDefault();
+                window.history.back();
+            } else if (thresholdX < diffX) {
+                // 右方向へ移動：進む
+                evt.preventDefault();
+                window.history.forward();
+            }
         }
-    }
-});
+    };
+    document.addEventListener("contextmenu", oncontextmenu);
+
+    return function () {
+        document.removeEventListener("mousedown", onmousedown);
+        document.removeEventListener("contextmenu", oncontextmenu);
+    };
+})();
